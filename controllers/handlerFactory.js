29c0189770f63aed2c36725df79f3505ee968efe
Apricot <83486785+Ayush-Thasale-007@users.noreply.github.com>
2022-07-20 16:36:11 +0530

    init --> Factory Handler

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,47 @@
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError("No document found with that ID", 404));
+    }
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError("No doc found with that ID", 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        doc,
+      },
+    });
+  });
+// exports.deleteTour = catchAsync(async (req, res, next) => {
+//     const tour = await Tour.findByIdAndDelete(req.params.id);
+
+//     if (!tour) {
+//       return next(new AppError("No tour found with that ID", 404));
+//     }
+
+//     res.status(204).json({
+//       status: "success",
+//       data: null,
+//     });
+//   });
